b7703977800327e84f0432137d9973d0ff19ca93
flippedCoin <157492552+sigmaCoderx@users.noreply.github.com>
2026-07-26 10:24:23 +0300

    added best practice of coding

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
 }
 
-
+//get your bot token from https://t.me/botfather
 bot = TeleBot("BOT_TOKEN",parse_mode="HTML")
 
 button = InlineKeyboardMarkup()
